e11d1adb9cedcac6d20c28c71f2a6ba014fbf739
carlosyanez <carlos.yanez.s@gmail.com>
2021-03-14 09:03:56 +0100

    +nsw

--- code/clean_lga.R
+++ code/clean_lga.R
@@ -1,4 +1,5 @@
-
+#' Function to "clean"/standardise LGA names for easy matching
+#' @param df dataframe with attribute 'LGA'
 clean_lga <-function(df){
   
   df %>%   mutate(LGA=stri_trans_totitle(tolower(LGA)),
@@ -26,13 +27,25 @@ clean_lga <-function(df){
                   LGA=str_remove(LGA,"Uninc"),
                   LGA=str_remove(LGA,"Region"),
                   LGA=str_remove(LGA,"Municipality"),
+                  LGA=str_remove(LGA,"Municipal"),
                   LGA=str_remove(LGA,"\\(East Arm\\)"),
+                  LGA=str_remove(LGA,"The The Of"),
+                  LGA=str_remove(LGA,"The Of"),
                   LGA=str_squish(LGA),
                   LGA=str_trim(LGA),
                   LGA=str_replace(LGA,"Un-Incorporated (Nhulunbuy) Area","Nhulunbuy"),
-                  LGA=str_replace(LGA,"George","George Town"))
+                  LGA=str_replace(LGA,"George","George Town"),
+                  LGA=str_replace(LGA,"Mid-Western al","Mid-Western"),
+                  LGA=str_replace(LGA,"The The Of Hornsby","Hornsby"),   
+                  LGA=str_replace(LGA,"Bathurst al","Bathurst"),                  
+                  LGA=str_replace(LGA,"Queanbeyan-Palerang","Queanbeyan–Palerang"),
+                  LGA=str_replace(LGA,"The The Of Kiama","Kiama"),
+                  LGA=str_replace(LGA,"The Of Sydney","Sydney"),
+                  LGA=if_else(str_length(LGA)==0,"Unincorporated",LGA)
+                  )
 
 }
+
 
 
 
