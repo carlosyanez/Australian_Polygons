247d34ca9f3df0bb4b08b1cc37ea4e1ac3b774be
carlosyanez <carlos.yanez.s@gmail.com>
2021-03-15 10:25:23 +0100

    function to copy to aussiemaps

--- code/merge_tables.R
+++ code/merge_tables.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 states <- c("vic","nsw","act","qld","nt","wa","sa","tas")
+aussie_maps_data <- "../aussiemaps/inst/extdata/"
 
 sa1_table <- map_dfr(states, function(x){
                     filename <- "sa1_table.csv"
@@ -7,4 +8,15 @@ sa1_table <- map_dfr(states, function(x){
 
 })
 
-write_csv(sa1_table,"sa1_table.csv")
+write_csv(sa1_table,str_c(aussie_maps_data,"aus_sa1_table.csv"))
+
+for(i in states){
+  
+          files <- dir(str_c(i,"/"),pattern=".rds")
+          files <- str_remove(files,".rds")
+
+          for(j in files){
+          object<-readRDS(str_c(i,"/",j,".rds"))
+          save(object,file=str_c(aussie_maps_data,i,"_",j,".rda"))
+          }
+}
