fd728f8c15c4f57156131b2ef98c751c5bfe4586
carlosyanez <carlos.yanez.s@gmail.com>
2021-03-15 12:43:51 +0100

    merge code

--- code/merge_tables.R
+++ code/merge_tables.R
@@ -1,14 +1,17 @@
 library(tidyverse)
 states <- c("vic","nsw","act","qld","nt","wa","sa","tas")
 aussie_maps_data <- "../aussiemaps/inst/extdata/"
+aussie_file_data <- "../aussiemaps/data/"
 
-sa1_table <- map_dfr(states, function(x){
+
+locations.table <- map_dfr(states, function(x){
                     filename <- "sa1_table.csv"
                     read_csv(str_c(x,"/",filename),col_types="ccccccccccccccccccd")
 
 })
 
-write_csv(sa1_table,str_c(aussie_maps_data,"aus_sa1_table.csv"))
+save(locations.table,file=str_c(aussie_file_data,"locations.rda"))
+
 
 for(i in states){
   
